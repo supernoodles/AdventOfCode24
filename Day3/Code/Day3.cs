e306af6ce3ed23d44e6351fd297c306460b8b7e6
Steve Cusack <supernoodles@users.noreply.github.com>
2025-01-04 11:56:28 +0000

    Day3 WIP

---
+++ Day3/Code/Day3.cs
@@ -0,0 +1,9 @@
+namespace Code;
+
+public class Day3
+{
+    public int Part1(string[] input)
+    {
+        return 0;
+    }
+}
---
+++ Day3/Code/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Day3!");
+
+var input = File.ReadAllLines("input.txt");
+
+var day3 = new Code.Day3();
+
+Console.WriteLine($"Part1 = {day3.Part1(input)}");
