f0f015a62cb803196f72c448296e902a173e224f
Steve Cusack <supernoodles@users.noreply.github.com>
2025-10-09 22:58:01 +0100

    Day 5 - Part 1

--- Day5/Code/Day5.cs
+++ Day5/Code/Day5.cs
@@ -4,6 +4,55 @@ public class Day5
 {
     public int Part1(string[] input)
     {
-        return 0;
+        Dictionary<string, List<string>> orderingRules = [];
+
+        List<List<string>> pageOrders = [];
+
+        foreach (var entry in input)
+        {
+            var items = entry?.Split('|');
+            if (items != null && items.Length == 2)
+            {
+                if (orderingRules.TryGetValue(items[0], out var orderList))
+                {
+                    orderingRules[items[0]].Add(items[1]);
+
+                    continue;
+                }
+
+                orderingRules.Add(items[0], [items[1]]);
+
+                continue;
+            }
+
+            var updates = entry?.Split(',');
+            if (updates != null && updates.Length > 1)
+            {
+                pageOrders.Add([.. updates]);
+            }
+        }
+
+        var middle = 0;
+
+        foreach (var order in pageOrders)
+        {
+            bool isValid = true;
+
+            for (int i = 0; i < order.Count - 1; ++i)
+            {
+                for (int j = i + 1; j < order.Count; ++j)
+                {
+                    if (!orderingRules.TryGetValue(order[i], out _) || !orderingRules[order[i]].Contains(order[j]))
+                    {
+                        isValid = false;
+                        continue;
+                    }
+                }
+            }
+
+            middle += isValid ? int.Parse(order[order.Count / 2]) : 0;
+        }
+
+        return middle;
     }
 }
--- Day5/Code/Program.cs
+++ Day5/Code/Program.cs
@@ -1,8 +1,41 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Day 5!");
 
+var testInput = """
+47|53
+97|13
+97|61
+97|47
+75|29
+61|13
+75|53
+29|13
+97|29
+53|29
+61|53
+97|53
+61|29
+47|13
+75|47
+97|75
+47|61
+75|61
+47|29
+75|13
+53|13
+
+75,47,61,53,29
+97,61,53,29,13
+75,29,13
+75,97,47,61,53
+61,13,29
+97,13,75,29,47
+""";
+
 var day5 = new Code.Day5();
 
 var input = File.ReadAllLines("input.txt");
+
+Console.WriteLine($"Part1={day5.Part1(testInput.Split("\n"))}");
 
 Console.WriteLine($"Part1={day5.Part1(input)}");
