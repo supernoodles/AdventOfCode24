d313a905013eef8b44fcbdabfad427b038cf0876
Steve Cusack <supernoodles@users.noreply.github.com>
2025-10-16 22:25:50 +0100

    Day 6 - part 2 wip

--- Day6/code/Day6.cs
+++ Day6/code/Day6.cs
@@ -1,3 +1,7 @@
+using System.Data;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
 namespace code;
 
 public class Day6
@@ -8,19 +12,24 @@ public class Day6
 
         PrintMap(map);
 
+        FindGuardRoute(map);
+
+        return map.SelectMany(row => row).Where(pos => pos == 'X').Count();
+    }
+
+    private void FindGuardRoute(char[][] map)
+    {
         var (guardX, guardY) = FindGuard(map);
 
         var velX = 0;
         var velY = -1;
 
-        var distinctPos = 0;
-
-        while (guardInBounds(map, guardX, guardY))
+        while (GuardInBounds(map, guardX, guardY))
         {
             var nextX = guardX + velX;
             var nextY = guardY + velY;
 
-            if (!guardInBounds(map, nextX, nextY))
+            if (!GuardInBounds(map, nextX, nextY))
             {
                 map[guardY][guardX] = 'X';
 
@@ -33,7 +42,7 @@ public class Day6
 
             if (next == '#')
             {
-                (velX, velY) = rotate90(velX, velY);
+                (velX, velY) = Rotate90(velX, velY);
                 continue;
             }
 
@@ -43,18 +52,107 @@ public class Day6
             guardX = nextX;
             guardY = nextY;
 
-            PrintMap(map);
+            //PrintMap(map);
         }
-
-        return map.SelectMany(row => row).Where(pos => pos == 'X').Count();
     }
 
-    private bool guardInBounds(char[][] map, int guardX, int guardY) =>
+    record Guard(int X, int Y, int VX, int VY);
+
+    public int Part2(string[] input)
+    {
+        char[][] guardMap = [.. input.Select(s => s.ToCharArray())];
+
+        var (guardInitX, guardInitY) = FindGuard(guardMap);
+
+        var (guardInitVelX, guardInitVelY) = (0, -1);
+
+        PrintMap(guardMap);
+
+        FindGuardRoute(guardMap);
+
+        var guardDefault = new Guard(-1, -1, -1, -1);
+
+        var guardLoop = 0;
+
+        for (int iX = 0; iX < guardMap[0].Length; ++iX)
+        {
+            for (int iY = 0; iY < guardMap.Length; ++iY)
+            {
+                Guard oGuard = guardDefault;
+
+                if (guardMap[iY][iX] != 'X')
+                {
+                    continue;
+                }
+
+                char[][] map = [.. input.Select(s => s.ToCharArray())];
+
+                // map[iY][iX] = 'O';
+                map[30][9] = 'O';
+
+                var (guardX, guardY, velX, velY) = (guardInitX, guardInitY, guardInitVelX, guardInitVelY);
+
+                while (GuardInBounds(map, guardX, guardY))
+                {
+                    var nextX = guardX + velX;
+                    var nextY = guardY + velY;
+
+                    if (!GuardInBounds(map, nextX, nextY))
+                    {
+                        guardX = nextX;
+                        guardY = nextY;
+
+                        continue;
+                    }
+
+                    var next = map[nextY][nextX];
+
+                    if (next == '#' || next == 'O')
+                    {
+                        (velX, velY) = Rotate90(velX, velY);
+
+                        if (next == '#')
+                        {
+                            continue;
+                        }
+
+                        if (oGuard == guardDefault)
+                        {
+                            oGuard = new Guard(guardX, guardY, velX, velY);
+                            continue;
+                        }
+
+                        if (oGuard == new Guard(guardX, guardY, velX, velY))
+                        {
+                            guardLoop += 1;
+                            break;
+                        }
+                    }
+
+                    map[guardY][guardX] = 'X';
+                    map[nextY][nextX] = '^';
+
+                    guardX = nextX;
+                    guardY = nextY;
+
+                    PrintMap(map);
+
+
+                    Thread.Sleep(100);
+                    //Console.ReadLine();
+                }
+            }
+        }
+
+        return guardLoop;
+    }
+
+    private bool GuardInBounds(char[][] map, int guardX, int guardY) =>
         guardX >= 0 && guardX < map[0].Length &&
         guardY >= 0 && guardY < map.Length;
 
 
-    private (int x, int y) rotate90(int x, int y) =>
+    private (int x, int y) Rotate90(int x, int y) =>
         (-y,
         x);
 
---
+++ Day6/tests/Part2Tests.cs
@@ -0,0 +1,25 @@
+﻿namespace tests;
+
+public class Part2Tests
+{
+    [Fact]
+    public void Test1()
+    {
+        var day6 = new code.Day6();
+
+        var input = """
+....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...
+""";
+
+        Assert.Equal(6, day6.Part2(input.Split("\n")));
+    }
+}
