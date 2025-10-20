efc0d35b5ef43ab3c81bf98c2d5133dad103fe83
Steve Cusack <supernoodles@users.noreply.github.com>
2025-10-20 19:25:05 +0100

    Day 9 - Part 1 wip

--- Day9/code/Day9.cs
+++ Day9/code/Day9.cs
@@ -1,9 +1,69 @@
+using System.Text;
+
 namespace code;
 
 public class Day9
 {
     public string Part1(string input)
     {
-        return "";
+        var blocksStr = input.Index().Aggregate(new StringBuilder(), (sb, id) =>
+        {
+            var output = id.Index % 2 == 0
+                ? $"{id.Index / 2}"
+                : ".";
+
+            for (int i = 0; i < int.Parse($"{id.Item}"); i++)
+            {
+                sb.Append(output);
+            }
+
+            return sb;
+        })
+        .ToString();
+
+        var blocks = input.Index()
+            .SelectMany(id =>
+            {
+                var output = id.Index % 2 == 0
+                    ? $"{id.Index / 2}"
+                    : ".";
+
+                return Enumerable.Repeat(output, int.Parse($"{id.Item}"));
+            })
+            .ToList();
+
+        int next = 0;
+
+        while (blocks[next] != ".")
+        {
+            ++next;
+        }
+
+        int last = blocks.Count - 1;
+
+        while (blocks[last] == ".")
+        {
+            --last;
+        }
+
+        while(next < last)
+        {
+            if (blocks[next] != ".")
+            {
+                ++next;
+                continue;
+            }
+
+            if (blocks[last] == ".")
+            {
+                --last;
+                continue;
+            }
+
+            blocks[next] = blocks[last];
+            blocks[last] = ".";
+        }
+
+        return blocksStr.ToString();
     }
 }
--- Day9/tests/Part1Tests.cs
+++ Day9/tests/Part1Tests.cs
@@ -8,7 +8,17 @@ public class Part1Tests
         var day9 = new code.Day9();
 
         var input = "12345";
-        
+
         Assert.Equal("0..111....22222", day9.Part1(input));
+    }
+
+    [Fact]
+    public void Test2()
+    {
+        var day9 = new code.Day9();
+
+        var input = "2333133121414131402";
+        
+        Assert.Equal("00...111...2...333.44.5555.6666.777.888899", day9.Part1(input));
     }
 }
