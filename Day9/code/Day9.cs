ccfaf820ed77917f79c189c312f6bd80f7f789d9
Steve Cusack <supernoodles@users.noreply.github.com>
2025-10-20 18:18:08 +0100

    Day 9 - wip

---
+++ Day9/code/Day9.cs
@@ -0,0 +1,9 @@
+namespace code;
+
+public class Day9
+{
+    public string Part1(string input)
+    {
+        return "";
+    }
+}
---
+++ Day9/tests/Part1Tests.cs
@@ -0,0 +1,14 @@
+﻿namespace tests;
+
+public class Part1Tests
+{
+    [Fact]
+    public void Test1()
+    {
+        var day9 = new code.Day9();
+
+        var input = "12345";
+        
+        Assert.Equal("0..111....22222", day9.Part1(input));
+    }
+}
