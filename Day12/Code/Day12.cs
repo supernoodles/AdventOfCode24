724895d08d208c955cb02f6fb1fcad39c70a10eb
Steve Cusack <supernoodles@users.noreply.github.com>
2025-10-27 22:21:44 +0000

    Day 12 - Part 1

--- Day12/Code/Day12.cs
+++ Day12/Code/Day12.cs
@@ -20,34 +20,40 @@ public static class Day12
                     continue;
                 }
 
-                totalPrice += Recurse(map, visited, map[row][col], row, col);
+                var area = 0;
+
+                var perim = Recurse(map, visited, map[row][col], row, col, ref area);
+
+                totalPrice += perim * area;
             }
         }
 
-        return 0;
+        return totalPrice;
     }
 
     private static bool IsInBounds(char[][] map, int row, int col) =>
         row >= 0 && row < map.Length && col >= 0 && col < map[0].Length;
 
-    private static int Recurse(char[][] map, HashSet<(int, int)> visited, char plant, int row, int col)
+    private static int Recurse(char[][] map, HashSet<(int, int)> visited, char plant, int row, int col, ref int area)
     {
-        if (visited.Contains((row, col)))
-        {
-            return 0;
-        }
-
         if (!IsInBounds(map, row, col) || map[row][col] != plant)
         {
             return 1;
         }
 
-        visited.Add((row, col));
+        if (visited.Contains((row, col)) && map[row][col] == plant)
+        {
+            return 0;
+        }
 
-        return Recurse(map, visited, plant, row + 1, col) +
-               Recurse(map, visited, plant, row - 1, col) +
-               Recurse(map, visited, plant, row, col + 1) +
-               Recurse(map, visited, plant, row, col - 1);
+        visited.Add((row, col));
+        area += 1;
+
+        return
+            Recurse(map, visited, plant, row - 1, col, ref area) +
+            Recurse(map, visited, plant, row, col + 1, ref area) +
+            Recurse(map, visited, plant, row + 1, col, ref area) +
+            Recurse(map, visited, plant, row, col - 1, ref area);
     }
 
     public static int Part2(string[] input)
--- Day12/Tests/Part1Tests.cs
+++ Day12/Tests/Part1Tests.cs
@@ -16,4 +16,40 @@ EEEC
 
         Assert.Equal(140, result);
     }
-}
+
+    [Fact]
+    public void Test2()
+    {
+        var input = """
+OOOOO
+OXOXO
+OOOOO
+OXOXO
+OOOOO
+""";
+
+        var result = Code.Day12.Part1(input.Split(Environment.NewLine));
+
+        Assert.Equal(772, result);
+    }
+
+[Fact]
+    public void Test3()
+    {
+        var input = """
+RRRRIICCFF
+RRRRIICCCF
+VVRRRCCFFF
+VVRCCCJFFF
+VVVVCJJCFE
+VVIVCCJJEE
+VVIIICJJEE
+MIIIIIJJEE
+MIIISIJEEE
+MMMISSJEEE
+""";
+
+        var result = Code.Day12.Part1(input.Split(Environment.NewLine));
+
+        Assert.Equal(1930, result);
+    }}
