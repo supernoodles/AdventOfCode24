4c12c557d3dccdf54879dab70a47766b60984712
Steve Cusack <supernoodles@users.noreply.github.com>
2025-11-17 22:59:39 +0000

    Day 15 - Part 1

--- Day15/Code/Day15.cs
+++ Day15/Code/Day15.cs
@@ -4,21 +4,14 @@ public class Day15
 {
     public static int Part1(string[] input)
     {
-        List<string> map = [];
+        List<char[]> map = [];
         List<string> movements = [];
-
-        var width = 0;
 
         foreach (var line in input.Where(_ => !string.IsNullOrWhiteSpace(_)).Select(_ => _.Trim()))
         {
             if (line.StartsWith('#'))
             {
-                map.Add(line);
-
-                if (width == 0)
-                {
-                    width = line.Length;
-                }
+                map.Add(line.ToCharArray());
 
                 continue;
             }
@@ -27,13 +20,76 @@ public class Day15
         }
 
         var height = map.Count;
+        var width = map[0].Length;
 
-        DisplayMap(map);
+        var (x, y) = FindGuard(map);
 
-        return 0;
+        var moves = movements.SelectMany(_ => _).ToList();
+
+        foreach (var move in moves)
+        {
+            //Console.WriteLine(move);
+
+            var (dx, dy) = move switch
+            {
+                '<' => (-1, 0),
+                '>' => (1, 0),
+                '^' => (0, -1),
+                'v' => (0, 1),
+                _ => throw new Exception("Bad")
+            };
+
+            if (TryPush(map, width, height, x, y, dx, dy))
+            {
+                (x, y) = (x+dx, y+dy);
+            }
+
+            //DisplayMap(map);
+        }
+
+        return map.Select((row, ri) => 
+            row.Select((col, i) => 
+                col == 'O' 
+                    ? ri * 100 + i 
+                    : 0)
+                .Sum()
+            ).Sum();
     }
 
-    private static void DisplayMap(List<string> map)
+    private static bool TryPush(List<char[]> map, int width, int height, int x, int y, int dx, int dy)
+    {
+        if (x < 0 || x >= width || y < 0 || y >= height || map[y][x] == '#')
+        {
+            return false;
+        }
+
+        if (map[y][x] == '.')
+        {
+            return true;
+        }
+
+        var moveNext = TryPush(map, width, height, x + dx, y + dy, dx, dy);
+
+        if (moveNext)
+        {
+            map[y + dy][x + dx] = map[y][x];
+            map[y][x] = '.';
+
+            return true;
+        }
+
+        return false;
+    }
+
+    private static (int x, int y) FindGuard(List<char[]> map)
+    {
+        var y = map.Index().Where(row => row.Item.Contains('@')).First().Index;
+        var x = map[y].Index().Where(col => col.Item.Equals('@')).First().Index;
+
+        return (x, y);
+    }
+
+    private static void DisplayMap(List<char[]> map)
     {
         foreach (var line in map)
         {
--- Day15/Tests/Part1Tests.cs
+++ Day15/Tests/Part1Tests.cs
@@ -19,4 +19,33 @@ public class Part1Tests
 """;
         Assert.Equal(2028, Code.Day15.Part1(input.Split(Environment.NewLine)));
     }
+
+    [Fact]
+    public void Test2()
+    {
+        var input = """
+##########
+#..O..O.O#
+#......O.#
+#.OO..O.O#
+#..O@..O.#
+#O#..O...#
+#O..O..O.#
+#.OO.O.OO#
+#....O...#
+##########
+
+<vv>^<v^>v>^vv^v>v<>v^v<v<^vv<<<^><<><>>v<vvv<>^v^>^<<<><<v<<<v^vv^v>^
+vvv<<^>^v^^><<>>><>^<<><^vv^^<>vvv<>><^^v>^>vv<>v<<<<v<^v>^<^^>>>^<v<v
+><>vv>v^v^<>><>>>><^^>vv>v<^^^>>v^v^<^^>v^^>v^<^v>v<>>v^v^<v>v^^<^^vv<
+<<v<^>>^^^^>>>v^<>vvv^><v<<<>^^^vv^<vvv>^>v<^^^^v<>^>vvvv><>>v^<<^^^^^
+^><^><>>><>^^<<^^v>>><^<v>^<vv>>v>>>^v><>^v><<<<v>>v<v<v>vvv>^<><<>^><
+^>><>^v<><^vvv<^^<><v<<<<<><^v<<<><<<^^<v<^^^><^>>^<v^><<<^>>^v<v^v<v^
+>^>>^v>vv>^<<^v<>><<><<v<<v><>v<^vv<<<>^^v^>^^>>><<^v>>v^v><^^>>^<>vv^
+<><^^>^^^<><vvvvv^v<v<<>^v<v>v<<^><<><<><<<^^<<<^<<>><<><^^^>^^<>^>v<>
+^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>
+v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^
+""";
+        Assert.Equal(10092, Code.Day15.Part1(input.Split(Environment.NewLine)));
+    }
 }
